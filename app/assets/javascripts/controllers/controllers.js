1e3b8ed8c9bf7940e584ab8cf7a219a6af253b42
Yaron <justame@gmail.com>
2014-03-11 02:55:24 +0200

    add navs

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -61,14 +61,6 @@ autoControllers.controller('ArticleCtrl', ['$scope', '$http', '$routeParams', '$
             }
         }).success(function(data) {
             $scope.article = data[0];
-
-            // var textArr = $scope.article.content.split('<iframe');
-            // console.log(textArr[0] + " :==: " + textArr[1]);
-
-            //textArr[1].replace('width="')
-
-            //$scope.trustedHtml = $sce.trustAsHtml($scope.article.content);
-            console.log($scope.article);
         });
     }
 ]);
